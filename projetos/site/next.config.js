b38aa89020034494e9ada66ad25347385a9b47cb
Camila <cruz.camila001@gmail.com>
2021-05-30 18:37:56 -0300

    docs: add storybook

--- projetos/site/next.config.js
+++ projetos/site/next.config.js
@@ -1,5 +1,5 @@
 const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')(['@camila-cruz/modulo-comum']);
+const withTM = require('next-transpile-modules')(['@camila-cruz/ui']);
 
 module.exports = withPlugins([withTM], {
   trailingSlash: true,
--- projetos/site/pages/index.tsx
+++ projetos/site/pages/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Button from '@camila-cruz/modulo-comum/components/Button';
+import Button from '@camila-cruz/ui/src/components/Button';
 
 const Title = styled.h1`
   color: red;
