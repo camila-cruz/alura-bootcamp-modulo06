33ec35c0acd2ae1a73abeb455e152e72e95b3d25
Camila <cruz.camila001@gmail.com>
2021-05-30 20:09:23 -0300

    feat: create storybook docs for Button

--- projetos/ui/src/components/Button/index.tsx
+++ projetos/ui/src/components/Button/index.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
-export default function Button() {
-  return <button>Um botão</button>
+interface ButtonProps {
+  /**
+   * Define se o botão pode receber eventos de clique
+   */
+  disabled?: boolean;
+  /**
+   * Conteúdo que será exibido dentro do botão
+   */
+  children: React.ReactNode;
+}
+
+export default function Button({ disabled, children }: ButtonProps) {
+  return (
+    <button disabled={disabled}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  disabled: false,
 }
