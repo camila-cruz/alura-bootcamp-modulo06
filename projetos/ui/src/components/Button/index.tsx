f7f8aa191fc4066a7ec3fcec9e3feb4d632a925d
Camila <cruz.camila001@gmail.com>
2021-06-03 18:07:50 -0300

    feat: add theme colors to the button

--- projetos/ui/src/components/Button/index.tsx
+++ projetos/ui/src/components/Button/index.tsx
@@ -1,6 +1,26 @@
+import { get } from 'lodash';
 import React from 'react';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.button`
+  border: 0;
+  cursor: pointer;
+  padding: 12px 26px;
+  font-weight: bold;
+  opacity: 1;
+  border-radius: 8px;
+
+  color: ${({ theme, color }) => get(theme, `colors.${color}.contrastText`)};
+  background-color: ${({ theme, color }) => get(theme, `colors.${color}.color`)};
+  
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.2;
+  }
+`;
 
 interface ButtonProps {
+  color: string;
   /**
    * Define se o botão pode receber eventos de clique
    */
@@ -11,14 +31,15 @@ interface ButtonProps {
   children: React.ReactNode;
 }
 
-export default function Button({ disabled, children }: ButtonProps) {
+export default function Button({ disabled, children, color }: ButtonProps) {
   return (
-    <button disabled={disabled}>
+    <ButtonWrapper disabled={disabled} color={color}>
       {children}
-    </button>
+    </ButtonWrapper>
   );
 }
 
 Button.defaultProps = {
+  color: 'primary.main',
   disabled: false,
 }
